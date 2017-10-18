8cdc6001062f50d1c48a58e56086931ea6b55b85
Luigi Doganieri <luigi5021@gmail.com>
2017-10-17 23:31:50 -0400

    addeing more permissions to cors module

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,10 +1,14 @@
-function cors(req, res, next) {
+function cors (req, res, next) {
   // we can define Only the routes we allow instead of all (*)
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header ('Access-Control-Allow-Origin', '*');
   // HTTP method allowed for CORS
-  res.header('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
+  res.header ('Access-Control-Allow-Credentials', 'true');
+  res.header ('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
+  res.header (
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+  );
+  next ();
 }
 
 module.exports = cors;
