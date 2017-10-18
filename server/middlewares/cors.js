087d5c3a974a7781d527d3aad30a83f96b5c2b3c
Luigi Doganieri <luigi5021@gmail.com>
2017-10-17 23:45:16 -0400

    added permission for x-auth in cors

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -6,7 +6,7 @@ function cors (req, res, next) {
   res.header ('Access-Control-Allow-Methods', 'GET,PATCH,PUT,POST,DELETE');
   res.header (
     'Access-Control-Allow-Headers',
-    'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
+    'Access-Control-Allow-Headers, Origin,Accept, x-auth, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers'
   );
   next ();
 }
